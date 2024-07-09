927b36dc71836b013dd1d61def079457bf99d5f9
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-07-10 00:01:09 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': ["Sary"],
+      },
+      colors: {
+        primary: "#004952",
+        primary100: "#FFF3EB",
+        secondary: "#1B9C73",
+        secondary100: "#EAF7E9",
+        dimWhite: "#EAF6FE",
+        grayBg: "#F4F3F1",
+        textC500: "#177EB3",
+      },
+      boxShadow: {
+        shadow100: "4px 3px 11.2px rgba(0, 0, 0, 0.25)",
+        shadow200: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
+
