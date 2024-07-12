65a7b71a53fbe59c6277482a8c42b1d3d8d4120b
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-07-12 19:21:33 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </BrowserRouter>,
   document.getElementById('root'),
 );
