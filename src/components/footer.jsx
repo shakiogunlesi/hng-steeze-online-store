d4562b5813c98b72abbeb4e8714bf1f1f69c477f
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-07-12 20:09:39 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <>
             <div className='my-10 rounded-[30px] py-10 md:px-16 px-6 bg-black flex gap-16 xl:flex-row lg:flex-row md:flex-row flex-col-reverse justify-between items-center mx-auto'>
                 <div className=" flex md:flex-col flex-col-reverse items-start gap-4 justify-start">
-                    <p className=" text-white md:text-[30px] text-[24px] font-OpenSans font-medium">Copyright © SteeZers 2024</p>
+                    <p className=" text-white md:text-[24px] text-[18px] font-[400]">Copyright © SteeZers 2024</p>
                     <div className=" flex flex-row gap-3 items-start justify-start">
                         <div className="flex flex-row justify-start items-start p-2 rounded-full bg-white">
                             <img 
@@ -34,7 +34,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <form className="flex flex-col gap-2" action="">
-                    <label className=" text-white md:text-[30px] text-[24px]">Subscribe to our Newsletter</label>
+                    <label className=" text-white md:text-[20px] text-[16px] font-[400]">Subscribe to our Newsletter</label>
                     <input type="email" placeholder="Enter your email here" className="bg-white text-gray-600 px-3 py-4 rounded-md " />
                     <button type="submit" className=" bg-black text-white border-white border rounded-lg py-2 px-4 w-fit h-fit"> Submit</button>
                 </form>
